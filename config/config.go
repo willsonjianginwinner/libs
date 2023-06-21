d302813a0bb3c69615782699ad011a58bd37f65e
willsonjianginwinner <willsonjianginwinner@gmail.com>
2023-06-22 00:45:42 +0800

    config 6/22

---
+++ config/config.go
@@ -0,0 +1,109 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path"
+	"runtime"
+	"sync"
+
+	"github.com/go-ini/ini"
+)
+
+var configMutex sync.RWMutex
+var envConfig *ini.File
+
+func init() {
+	var prefix string
+	_, filename, _, ok := runtime.Caller(0)
+	if ok {
+		prefix = path.Dir(filename) + "/"
+	}
+
+	err := Set(prefix, nil)
+
+	if err != nil {
+		log.Fatalln(err)
+		os.Exit(1)
+		return
+	}
+}
+
+func Set(prefix string, files []any) error {
+	iniFile, err := loadConfig(prefix, files)
+
+	if err != nil {
+		return fmt.Errorf("fail to read file: %v", err)
+	}
+
+	configMutex.Lock()
+	defer configMutex.Unlock()
+	envConfig = iniFile
+	return nil
+}
+
+func loadConfig(prefix string, files []any) (*ini.File, error) {
+
+	for idx, val := range files {
+		if config, ok := val.(string); ok {
+			config = prefix + config
+			files[idx] = config
+		}
+	}
+
+	iniFile, err := ini.Load(prefix+"config/env.ini", files...)
+
+	return iniFile, err
+}
+
+func Get(section, key, defaultValue string) string {
+	configMutex.RLock()
+	defer configMutex.RUnlock()
+
+	var value = defaultValue
+	if v := envConfig.Section(section).Key(key).String(); v != "" {
+		value = v
+	}
+
+	return value
+}
+
+func GetBool(section, key string, defaultValue bool) bool {
+	configMutex.RLock()
+	defer configMutex.RUnlock()
+
+	var value = defaultValue
+
+	if v, err := envConfig.Section(section).Key(key).Bool(); nil == err {
+		value = v
+	}
+
+	return value
+}
+
+func GetInt(section, key string, defaultValue int) int {
+	configMutex.RLock()
+	defer configMutex.RUnlock()
+
+	var value = defaultValue
+
+	if v, err := envConfig.Section(section).Key(key).Int(); nil == err {
+		value = v
+	}
+
+	return value
+}
+
+func GetInt64(section, key string, defaultValue int64) int64 {
+	configMutex.RLock()
+	defer configMutex.RUnlock()
+
+	var value = defaultValue
+
+	if v, err := envConfig.Section(section).Key(key).Int64(); nil == err {
+		value = v
+	}
+
+	return value
+}
